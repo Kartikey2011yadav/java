4e2f487901a90b8ae3dbc9f7b227e2a0175194f6
Kartikey2011yadav <nykay10968@gmail.com>
2024-02-01 19:23:30 +0530

    ada stack prog..

---
+++ src/ADA/EmployStack.java
@@ -0,0 +1,163 @@
+package ADA;
+
+import java.util.Scanner;
+
+class Node{
+    private Node n = null;
+    private String Name;
+    private int id;
+    private String position;
+    private double salary;
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Node getN() {
+        return n;
+    }
+
+    public void setN(Node n) {
+        this.n = n;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
+
+public class EmployStack {
+    Node head = null;
+    void push(String n, int i, String pos, double s){
+        if(head == null){
+            Node emp = new Node();
+            emp.setId(i);
+            emp.setName(n);
+            emp.setPosition(pos);
+            emp.setSalary(s);
+            head = emp;
+        }
+        else {
+            Node emp = new Node();
+            emp.setId(i);
+            emp.setName(n);
+            emp.setN(head);
+            emp.setPosition(pos);
+            emp.setSalary(s);
+            head = emp;
+        }
+    }
+    void pop(){
+        if(head != null){
+            System.out.println("Employee: "+head.getName()+" Id: "+head.getId());
+            System.out.println("Employee Salary: "+head.getSalary()+" Position: "+head.getPosition());
+            head = head.getN();
+        }
+        else{
+            System.out.println("Stack is empty");
+        }
+    }
+    void peek(){
+        if(head != null){
+            System.out.println("Employee: "+head.getName()+" Id: "+head.getId());
+            System.out.println("Employee Salary: "+head.getSalary()+" Position: "+head.getPosition());
+        }
+        else{
+            System.out.println("Stack is empty");
+        }
+    }
+    void display(){
+        Node itr = head;
+        while(itr != null){
+            System.out.println("Employee: "+itr.getName()+" Id: "+itr.getId());
+            System.out.println("Employee Salary: "+itr.getSalary()+" Position: "+itr.getPosition());
+            itr = itr.getN();
+        }
+    }
+
+    void test(EmployStack e){
+        e.push("Nathkhat",25,"Assistant",50000.68);
+        e.push("Nathkhat23",25,"Assistant",50000.68);
+        e.push("Nathkhat45",25,"Assistant",50000.68);
+        e.push("Nathkhat465",25,"Assistant",50000.68);
+        e.push("Nathkhat7",25,"Assistant",50000.68);
+        e.push("Nathkhat8",25,"Assistant",50000.68);
+        e.display();
+        e.pop();
+        e.pop();
+        e.peek();
+        e.pop();
+        e.pop();
+        e.pop();
+        e.pop();
+    }
+
+    public static void main(String[] args) {
+        EmployStack e = new EmployStack();
+        Scanner sc = new Scanner(System.in);
+//        e.test(e);
+        boolean quit = false;
+        while(!quit){
+            System.out.println("-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x");
+            System.out.println("Insert New Employee: 1");
+            System.out.println("Remove a Employee: 2");
+            System.out.println("Top Employee: 3");
+            System.out.println("Display all Employees: 4");
+            System.out.println("Exit: 5");
+            System.out.print("Enter a option: ");
+            switch (sc.nextInt()){
+                case 1:
+                    System.out.println("Enter Employee Name: ");
+                    String n = sc.next();
+                    System.out.println("Enter Employee position: ");
+                    String pos = sc.next();
+                    System.out.println("Enter Employee ID: ");
+                    int i = sc.nextInt();
+                    System.out.println("Enter Employee Salary: ");
+                    double s = sc.nextDouble();
+                    e.push(n,i,pos,s);
+                    break;
+
+                case 2:
+                    e.pop();
+                    break;
+
+                case 3:
+                    e.peek();
+                    break;
+
+                case 4:
+                    e.display();
+                    break;
+
+                case 5:
+                    quit = true;
+                    break;
+            }
+        }
+    }
+}
