0538719b5ca489da2a1ddcc1aefbbbf055db2e73
Kartikey2011yadav <nykay10968@gmail.com>
2024-02-07 18:03:47 +0530

    Dining Philosopher Problem update

--- src/AdvanceJava/MultiThreading/DiningPhilosopher.java
+++ src/AdvanceJava/MultiThreading/DiningPhilosopher.java
@@ -1,11 +1,14 @@
 package AdvanceJava.MultiThreading;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class DiningPhilosopher {
 
-    static int philosopher = 5;
+    static int philosopher = 8;
+    static int chances = 20;
     static philosopher[] philosophers = new philosopher[philosopher];
     static chopstick[] chopsticks = new chopstick[philosopher];
 
@@ -48,21 +51,25 @@ public class DiningPhilosopher {
 
             while (true) {
                 leftchopstick.grab();
-                System.out.println("philosopher " + (number+1) + " grabs left chopstick.");
+//                System.out.println("philosopher " + (number+1) + " grabs left chopstick.");
                 rightchopstick.grab();
-                System.out.println("philosopher " + (number+1) + " grabs right chopstick.");
+//                System.out.println("philosopher " + (number+1) + " grabs right chopstick.");
                 eat();
                 leftchopstick.release();
-                System.out.println("philosopher " + (number+1) + " releases left chopstick.");
+//                System.out.println("philosopher " + (number+1) + " releases left chopstick.");
                 rightchopstick.release();
-                System.out.println("philosopher " + (number+1) + " releases right chopstick.");
+//                System.out.println("philosopher " + (number+1) + " releases right chopstick.");
             }
         }
 
         void eat() {
             try {
-                int sleepTime = ThreadLocalRandom.current().nextInt(0, 1000);
-                System.out.println("philosopher " + (number+1) + " eats for " + sleepTime);
+                int sleepTime = ThreadLocalRandom.current().nextInt(1000, 3000);
+                System.out.println("philosopher " + (number+1) + " eats for " + ((double)sleepTime/1000)+" sec");
+                chances -=1;
+                if(chances<=0){
+                    System.exit(0);
+                }
                 Thread.sleep(sleepTime);
             }
             catch (Exception e) {
@@ -74,41 +81,23 @@ public class DiningPhilosopher {
 
     public static void main(String[] argv) {
 
-        for (int i = 0; i < philosopher; i++) {
+        int no_of_philosophers =8;
+        for (int i = 0; i < no_of_philosophers; i++){
             chopsticks[i] = new chopstick();
         }
+        ExecutorService executor =
+                Executors.newFixedThreadPool(no_of_philosophers);
+        for (int i = 0; i < no_of_philosophers; i++){
+            if( i % 2 == 0)
+                philosophers[i] = new philosopher(i, chopsticks[(i
+                        + 1) % no_of_philosophers], chopsticks[i] );
+            else
+                philosophers[i] = new philosopher(i,
+                        chopsticks[i], chopsticks[(i + 1) %
+                        no_of_philosophers] );
 
-        for (int i = 0; i < philosopher; i++) {
-            philosophers[i] = new philosopher(i, chopsticks[i], chopsticks[(i + 1) % philosopher]);
-            philosophers[i].start();
+            executor.execute(philosophers[i]);
         }
-
-        while (true) {
-            try {
-                // sleep 1 sec
-                Thread.sleep(1000);
-
-                // check for deadlock
-                boolean deadlock = true;
-                for (chopstick f : chopsticks) {
-                    if (f.isFree()) {
-                        deadlock = false;
-                        break;
-                    }
-                }
-                if (deadlock) {
-                    Thread.sleep(1000);
-                    System.out.println("Everyone Eats");
-                    break;
-                }
-            }
-            catch (Exception e) {
-                e.printStackTrace(System.out);
-            }
-        }
-
-        System.out.println("Exit The Program!");
-        System.exit(0);
     }
 
 }
