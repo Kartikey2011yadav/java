b72886628513646332ecb70df69bea5c1e9cbd7e
Kartikey2011yadav <nykay10968@gmail.com>
2024-02-08 20:57:52 +0530

    SequentialExecution update

---
+++ src/AdvanceJava/MultiThreading/SequentialExecution.java
@@ -0,0 +1,28 @@
+package AdvanceJava.MultiThreading;
+
+public class SequentialExecution {
+    static class A implements Runnable{
+        @Override
+        public void run() {
+
+        }
+    }
+
+    static class B implements Runnable{
+        @Override
+        public void run() {
+
+        }
+    }
+
+    static class C implements Runnable{
+        @Override
+        public void run() {
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
