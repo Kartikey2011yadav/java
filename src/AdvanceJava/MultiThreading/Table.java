996d04b3e9cc627e46961e325d9419f15889fd46
Kartikey2011yadav <nykay10968@gmail.com>
2024-02-10 19:50:45 +0530

    update

---
+++ src/AdvanceJava/MultiThreading/Table.java
@@ -0,0 +1,8 @@
+package AdvanceJava.MultiThreading;
+
+public class Table {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/AdvanceJava/multithreading/Buffer.java
+++
@@ -1,7 +0,0 @@
-package AdvanceJava.multithreading;
-
-public interface Buffer {
-    void set(int value); // place int value into Buffer
-
-    int get(); // return int value from Buffer
-}
