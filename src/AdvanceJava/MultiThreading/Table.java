b88c5d69c416e2d0913c6cabbe3ea8633dec276f
Kartikey2011yadav <nykay10968@gmail.com>
2024-02-11 08:52:59 +0530

    update

--- src/AdvanceJava/MultiThreading/Table.java
+++ src/AdvanceJava/MultiThreading/Table.java
@@ -1,8 +1,32 @@
 package AdvanceJava.MultiThreading;
 
-public class Table {
+import java.util.HashMap;
+import java.util.Map;
 
-    public static void main(String[] args) {
+public class Table implements Runnable {
+    static Map<Integer,Integer> table = new HashMap<>();
+    int index;
 
+    public Table(int index) {
+        this.index = index;
+    }
+
+    @Override
+    public void run() {
+        table.put(index,index*13);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Table[] threads = new Table[20];
+        for (int i = 0; i < 20; i++) {
+            threads[i] = new Table(i+1);
+            Thread t = new Thread(threads[i]);
+            t.start();
+        }
+        Thread.sleep(1500);
+        System.out.println("Table of 12: ");
+        table.forEach((k,v)->{
+            System.out.println(k+" x 12 = "+v);
+        });
     }
 }
