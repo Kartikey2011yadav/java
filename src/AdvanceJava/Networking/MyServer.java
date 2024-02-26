4677f25771b0a65c3c47ef422310cfdafad42146
Kartikey2011yadav <nykay10968@gmail.com>
2024-02-26 21:03:02 +0530

    MyServer update

--- src/AdvanceJava/Networking/MyServer.java
+++ src/AdvanceJava/Networking/MyServer.java
@@ -9,7 +9,8 @@ public class MyServer {
         try{
             ServerSocket ss = new ServerSocket(6666);
             Socket s = ss.accept();
-            DataInputStream dis = new DataInputStream(s.getInputStream());
+            DataInputStream dis = new DataInputStream(
+                    s.getInputStream());
             String str = (String) dis.readUTF();
             System.out.println("Message: "+ str);
             ss.close();
