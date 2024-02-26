0ddd499d8f1e94555fce96e8e245a6d6ad4e3497
Kartikey2011yadav <nykay10968@gmail.com>
2024-02-26 21:03:21 +0530

    MyClient update

--- src/AdvanceJava/Networking/MyClient.java
+++ src/AdvanceJava/Networking/MyClient.java
@@ -7,7 +7,8 @@ public class MyClient {
     public static void main(String[] args) {
         try{
             Socket s =new Socket("localhost",6666);
-            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            DataOutputStream dout = new DataOutputStream(
+                    s.getOutputStream());
             dout.writeUTF("Hello Server");
             dout.flush();
             dout.close();
