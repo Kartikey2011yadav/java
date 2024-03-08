96aa39c959ef57d41bf1afdc73494c01231049c4
Kartikey2011yadav <nykay10968@gmail.com>
2024-03-08 23:49:51 +0530

    extras

--- src/AdvanceJava/Networking/URLClass.java
+++ src/AdvanceJava/Networking/URLClass.java
@@ -5,7 +5,7 @@ import java.net.URL;
 public class URLClass {
     public static void main(String[] args) {
         try{
-            URL url = new URL("https://mangapill.com/chapters/5343-10139000/mokushiroku-no-yonkishi-chapter-139");
+            URL url = new URL("https://mangapill.com/chapters/2085-10243000/jujutsu-kaisen-chapter-243");
 
             System.out.println(url.getProtocol());
             System.out.println(url.getHost());
