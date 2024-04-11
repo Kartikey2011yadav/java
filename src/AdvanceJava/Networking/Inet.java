285550e7d26a05ca22cc06574d95cf1d7cbcd7f8
Kartikey2011yadav <nykay10968@gmail.com>
2024-04-11 15:33:31 +0530

    Extras

--- src/AdvanceJava/Networking/Inet.java
+++ src/AdvanceJava/Networking/Inet.java
@@ -2,7 +2,6 @@ package AdvanceJava.Networking;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Arrays;
 
 public class Inet {
     public static void main(String[] args) throws UnknownHostException {
@@ -10,7 +9,5 @@ public class Inet {
         System.out.println(inet.getHostAddress());
         System.out.println(inet.getCanonicalHostName());
         System.out.println(inet.getHostName());
-        String s = "Heloooretyreghutygheruytuhuehgtuyehgr uykhgt ueyrtuyhyerguytr ";
-        System.out.println(Arrays.toString(s.getBytes()));
     }
 }
