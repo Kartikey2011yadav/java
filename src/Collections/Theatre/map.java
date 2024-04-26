3c1c0c637bd94d00a1f84ac56f4337352cd4d853
Kartikey2011yadav <nykay10968@gmail.com>
2024-04-27 05:25:06 +0530

    map

---
+++ src/Collections/Theatre/map.java
@@ -0,0 +1,13 @@
+package Collections.Theatre;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class map {
+    public static void main(String[] args) {
+        Map<Integer,Integer> m = new HashMap<>();
+        m.put(2,4);
+        m.put(3,7);
+        System.out.println(m);
+    }
+}
