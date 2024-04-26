17c5720425ab29033372c82a2af417463f1241f7
Kartikey2011yadav <nykay10968@gmail.com>
2024-04-27 05:24:45 +0530

    SetDemo

---
+++ src/Collections/Theatre/SetDemo.java
@@ -0,0 +1,15 @@
+package Collections.Theatre;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class SetDemo {
+    public static void main(String[] args) {
+        SortedSet<String> s = new TreeSet<>();
+        s.add("djhsagfhdjk");
+        s.add("Ewfhjk");
+        s.add("10");
+        s.add("2");
+        System.out.println(s);
+    }
+}
